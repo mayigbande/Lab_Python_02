068168c2de85b786a98e36856f655bb9a0146f5a
Mayi Gbande <pcspida@ubuntu.(none)>
2012-06-21 12:28:23 +0000

    python_02

---
+++ UsingControlStructures.py
@@ -0,0 +1,7 @@
+theInput =raw_input("Enter an integer: ")
+number=int(theInput)
+#Your cod here
+if number%2==0:
+    print number,"is even"
+else:
+    print number,"is odd "
---
+++ userinput.py
@@ -0,0 +1,10 @@
+firstname=raw_input('Enter your first name:')
+lastname=raw_input('Enter your last name:')
+birth='Enter your date of birth:'
+
+print birth
+month=raw_input('Month?')
+day=raw_input('Day?')
+year=raw_input('year?')
+print firstname,lastname,month,day,year
+print firstname,lastname,'was born on',month,day,',',year
